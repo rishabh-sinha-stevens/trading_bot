5c6d8f97d4fff3457af3c335d653753b6f523fe7
Prapti Patel <praptij1806@gmail.com>
2025-04-29 17:32:06 -0400

    Add SentimentBot class with init and method stub

--- SentimentBot.py
+++ SentimentBot.py
@@ -3,3 +3,10 @@ from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 from textblob import TextBlob
 import pandas as pd
 
+class SentimentBot:
+    def __init__(self):
+        # initialize a single VADER analyzer instance
+        self.vader = SentimentIntensityAnalyzer()
+
+    def process_news_sentiment(self, ticker_symbol: str) -> pd.DataFrame:
+        pass
